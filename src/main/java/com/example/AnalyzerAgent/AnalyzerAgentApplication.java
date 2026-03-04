9e5f6b86ef07564094868a5068532b25d723c669
Raj <ayush.l.raj@accenture.com>
2026-03-04 18:03:35 +0530

    Added Agent Config

---
+++ src/main/java/com/example/AnalyzerAgent/AnalyzerAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnalyzerAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerAgentApplication.class, args);
+	}
+
+}
