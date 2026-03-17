fcc7972fe3db3445c5e7bccfebb71a91956d63a2
Raj <ayush.l.raj@accenture.com>
2026-03-17 14:18:18 +0530

    Added Logs

--- src/main/java/com/example/AnalyzerAgent/AnalyzerAgentApplication.java
+++ src/main/java/com/example/AnalyzerAgent/AnalyzerAgentApplication.java
@@ -1,16 +1,21 @@
 package com.example.AnalyzerAgent;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class AnalyzerAgentApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(AnalyzerAgentApplication.class, args);
 		System.out.println("Starting Analysis");
+		try{
 		SnykReporterParser.parse();
 		SonarReportParser.parse();
+		}
+		catch(Exception e){
+			System.err.println("Error");
+			e.printStackTrace();
+		}
 		System.out.println("Analysis Completed");
+		System.exit(0);
 	}
 
 	
--- src/test/java/com/example/AnalyzerAgent/AnalyzerAgentApplicationTests.java
+++ src/test/java/com/example/AnalyzerAgent/AnalyzerAgentApplicationTests.java
@@ -1,12 +1,8 @@
 package com.example.AnalyzerAgent;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AnalyzerAgentApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
